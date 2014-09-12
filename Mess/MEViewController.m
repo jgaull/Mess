5fe8c0fbfe9e54327d38ee7bdf47be39fc1d9a3a
Jonathan Gaull <jonbeesh@gmail.com>
2014-09-11 20:09:33 -0700

    ?

---
+++ Mess/MEViewController.m
@@ -0,0 +1,32 @@
+//
+//  MEViewController.m
+//  Mess
+//
+//  Created by Jon on 8/21/14.
+//  Copyright (c) 2014 Modeo. All rights reserved.
+//
+
+#import <ModeoFramework/ModeoController.h>
+
+#import "MEViewController.h"
+
+@interface MEViewController ()
+
+@end
+
+@implementation MEViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
